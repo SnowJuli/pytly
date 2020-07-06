7c421dae8f64ba05f142c3e41d35a1aa23073aba
Quoorex <46283604+Quoorex@users.noreply.github.com>
2020-07-06 10:30:26 +0200

    Remove obsolete code

--- start.py
+++ start.py
@@ -5,29 +5,6 @@ from src.expand.expand import expandBitlink
 from src.information.information import getInformation
 
 
-def question():
-    print("""Pytly - Made by Snowleoo
-------------------------------------
-1. Shorten a Link
-2. Expand a Bitlink
-3. Get informations of a Bitlink
-4. Exit the Script""")
-
-    option = input("Enter a number: ")
-
-    if option == "1":
-        createBitlink()
-
-    if option == "2":
-        expandBitlink()
-
-    if option == "3":
-        getInformations()
-
-    if option == "4" or "exit":
-        pass
-
-
 print("""Pytly - Made by Snowleoo
 ------------------------------------""")
 
