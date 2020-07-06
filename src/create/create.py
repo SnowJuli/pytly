8c3c5462621c4da8f431a9d370ef12b71102ee77
Quoorex <46283604+Quoorex@users.noreply.github.com>
2020-07-06 11:12:25 +0200

    Add options menu

--- src/create/create.py
+++ src/create/create.py
@@ -1,5 +1,32 @@
+from PyInquirer import prompt
 import requests
 
 
 def createBitlink():
-    r = requests.post("https://api-ssl.bitly.com/v4/shorten")
+    questions = [
+        {
+            'type': 'input',
+            'name': 'long_url',
+            'message': 'URL you want to shorten: ',
+        },
+        {
+            'type': 'input',
+            'name': 'domain',
+            'message': 'Domain to use for the shortened URL: ',
+            "default": "bit.ly",
+        },
+        {
+            'type': 'input',
+            'name': 'group_guid',
+            'message': 'Bit.ly group GUID: ',
+            "default": "",
+        },
+    ]
+    options = prompt(questions)
+    headers = {
+        "group_guid": options["group_guid"],
+        "domain": options["domain"],
+        "long_url": options["long_url"]
+    }
+    r = requests.post("https://api-ssl.bitly.com/v4/shorten", headers=headers)
+    print(r.text)
