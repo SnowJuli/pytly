c8894fcb59b920476b894ea9e4fc62b21bfaef30
Quoorex <46283604+Quoorex@users.noreply.github.com>
2020-07-06 11:59:17 +0200

    Add code to create Bitlinks

--- src/create/create.py
+++ src/create/create.py
@@ -18,15 +18,17 @@ def createBitlink():
         {
             'type': 'input',
             'name': 'group_guid',
-            'message': 'Bit.ly group GUID: ',
-            "default": "",
+            'message': 'Bit.ly group GUID (default=None): ',
         },
     ]
     options = prompt(questions)
     headers = {
+        "Authorization": "Bearer api_token",
+    }
+    json = {
         "group_guid": options["group_guid"],
         "domain": options["domain"],
         "long_url": options["long_url"]
     }
-    r = requests.post("https://api-ssl.bitly.com/v4/shorten", headers=headers)
-    print(r.text)
+    r = requests.post("https://api-ssl.bitly.com/v4/shorten", headers=headers, json=json).json()
+    print(f"Created the Bitlink '{r['link']}' pointing to '{r['long_url']}' and the ID '{r['id']}'")
